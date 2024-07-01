7842319cd569ac5ed5db608e56aead0bbc2f8bfc
josé <jg345762@gmail.com>
2024-07-01 15:31:34 -0300

    cors updated

--- src/main/java/com/aluralatam/forohub/infra/configuration/CorsConfiguration.java
+++ src/main/java/com/aluralatam/forohub/infra/configuration/CorsConfiguration.java
@@ -1,19 +1,15 @@
-import org.springframework.context.annotation.Bean;
+package com.aluralatam.forohub.infra.configuration;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedOrigins("*")
-                        .allowedHeaders("*");
-            }
-        };
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }

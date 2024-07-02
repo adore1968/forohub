58a855121d48a6dea60a2ac88f9fb33a9b5625cd
josé <jg345762@gmail.com>
2024-07-02 14:54:44 -0300

    swagger ui implemented

--- src/main/java/com/aluralatam/forohub/controllers/TopicosController.java
+++ src/main/java/com/aluralatam/forohub/controllers/TopicosController.java
@@ -2,6 +2,8 @@ package com.aluralatam.forohub.controllers;
 
 import com.aluralatam.forohub.domain.topicos.TopicosService;
 import com.aluralatam.forohub.domain.topicos.dto.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import jakarta.websocket.server.PathParam;
@@ -23,6 +25,8 @@ public class TopicosController {
     @Autowired
     private TopicosService topicosService;
 
+    @Tag(name = "post", description = "Metodos POST de la API de Topicos")
+    @Operation(summary = "Registrar un topico", description = "Registrar un topico. La respuesta es ese mismo topico creado con su respectivo id, titulo, mensaje, fecha de creacion, status, nombre de curso y el id del autor")
     @PostMapping
     @Transactional
     public ResponseEntity<DatosDetallesTopico> registrarTopico(@RequestBody @Valid DatosRegistroTopico datosRegistroTopico, UriComponentsBuilder uriComponentsBuilder) {
@@ -31,18 +35,24 @@ public class TopicosController {
         return ResponseEntity.created(url).body(new DatosDetallesTopico(topico));
     }
 
+    @Tag(name = "get", description = "Metodos GET de la API de Topicos")
+    @Operation(summary = "Listar topicos", description = "Listar 5 topicos ordenados por su titulo. La respuesta es una lista de topicos con su respectivo id, titulo, mensaje, fecha de creacion, status, nombre de curso y el correo electronico del autor")
     @GetMapping
     public ResponseEntity<Page<DatosListadoTopico>> listarTopicos(@PageableDefault(size = 5, sort = "titulo") Pageable pageable) {
         var topicos = topicosService.obtenerTopicos(pageable).map(DatosListadoTopico::new);
         return ResponseEntity.ok(topicos);
     }
 
+    @Tag(name = "get", description = "Metodos GET de la API de Topicos")
+    @Operation(summary = "Listar topicos por su fecha", description = "Listar 10 topicos ordenados por su fecha de creacion. La respuesta es una lista de topicos con su respectivo id, titulo, mensaje, fecha de creacion, status, nombre de curso y el correo electronico del autor")
     @GetMapping("/filtrar1")
     public ResponseEntity<Page<DatosListadoTopico>> listarTopicosPorFecha(@PageableDefault(size = 10, sort = "fechaDeCreacion") Pageable pageable) {
         var topicos = topicosService.obtenerTopicos(pageable).map(DatosListadoTopico::new);
         return ResponseEntity.ok(topicos);
     }
 
+    @Tag(name = "get", description = "Metodos GET de la API de Topicos")
+    @Operation(summary = "Listar topicos por curso y año", description = "Listar topicos ordenados por un año especifico y nombre de curso. La respuesta es una lista de topicos con su respectivo id, titulo, mensaje, fecha de creacion, status, nombre de curso y el correo electronico del autor")
     @GetMapping("/filtrar2")
     public ResponseEntity<List<DatosListadoTopico>> listarTopicosPorCursoYAño(@RequestParam String curso, @RequestParam Integer año) {
         var topicos = topicosService.obtenerTopicosConParam(curso, año).stream()
@@ -51,12 +61,16 @@ public class TopicosController {
         return ResponseEntity.ok(topicos);
     }
 
+    @Tag(name = "get", description = "Metodos GET de la API de Topicos")
+    @Operation(summary = "Detallar topico", description = "Encontrar y detallar topico por su id. La respuesta es el topico encontrado con su respectivo id, titulo, mensaje, fecha de creacion, status, nombre de curso y el correo electronico del autor")
     @GetMapping("/{id}")
     public ResponseEntity<DatosRespuestaTopico> detallarTopico(@PathVariable Long id) {
         var topico = topicosService.obtenerTopico(id);
         return ResponseEntity.ok(new DatosRespuestaTopico(topico));
     }
 
+    @Tag(name = "put", description = "Metodos PUT de la API de Topicos")
+    @Operation(summary = "Actualizar topico", description = "Encontrar y actualizar topico por su id. La respuesta es el topico actualizado con su respectivo id, titulo, mensaje, fecha de creacion, status, nombre de curso y el correo electronico del autor")
     @PutMapping("/{id}")
     @Transactional
     public ResponseEntity<DatosRespuestaTopico> actualizarTopico(@PathVariable Long id, @RequestBody DatosActualizarTopico datosActualizarTopico) {
@@ -64,6 +78,8 @@ public class TopicosController {
         return ResponseEntity.ok(new DatosRespuestaTopico(topico));
     }
 
+    @Tag(name = "delete", description = "Metodos DELETE de la API de Topicos")
+    @Operation(summary = "Eliminar", description = "Encontrar y eliminar topico por su id")
     @DeleteMapping("/{id}")
     @Transactional
     public ResponseEntity eliminarTopico(@PathVariable Long id) {
--- src/main/java/com/aluralatam/forohub/infra/config/CorsConfiguration.java
+++ src/main/java/com/aluralatam/forohub/infra/config/CorsConfiguration.java
@@ -1,4 +1,4 @@
-package com.aluralatam.forohub.infra.configuration;
+package com.aluralatam.forohub.infra.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
